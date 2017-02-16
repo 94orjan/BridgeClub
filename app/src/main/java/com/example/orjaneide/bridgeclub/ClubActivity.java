64c7818823056988c7aaa1df1536bb574ba5285a
Ørjan Eide <orjaneide@live.no>
2017-02-16 16:41:21 +0100

    Made new ClubActivity

--- app/src/main/java/com/example/orjaneide/bridgeclub/ClubActivity.java
+++ app/src/main/java/com/example/orjaneide/bridgeclub/ClubActivity.java
@@ -7,13 +7,39 @@ import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ClubActivity extends AppCompatActivity {
+
+    private ImageButton phonecallButton;
+    private ImageButton emailButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_club);
+
+        final int phoneNumber = 99442211;
+
+        final String email = "melk@gmail.com";
+
+        phonecallButton = (ImageButton) findViewById(R.id.phone_call_imageButton);
+        emailButton = (ImageButton) findViewById(R.id.email_imageButton);
+
+        phonecallButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makePhoneCallIntent(phoneNumber);
+            }
+        });
+
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmailIntent(email);
+            }
+        });
     }
 
     private void makePhoneCallIntent(int phoneNumer) {
