cc4da9ababd3b8889ee53234fa254922ea0389d7
Ørjan Eide <orjaneide@live.no>
2017-02-19 16:27:34 +0100

    styled the ClubActivity layout

--- app/src/main/java/com/example/orjaneide/bridgeclub/ClubActivity.java
+++ app/src/main/java/com/example/orjaneide/bridgeclub/ClubActivity.java
@@ -1,18 +1,15 @@
 package com.example.orjaneide.bridgeclub;
 
-import android.*;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.orjaneide.bridgeclub.model.Club;
 import com.example.orjaneide.bridgeclub.model.ClubDao;
@@ -22,12 +19,14 @@ public class ClubActivity extends AppCompatActivity {
     public static final String CLUB_NUMBER = "CLUB_NUMBER";
     private static final String TAG = ClubActivity.class.getSimpleName();
 
-    private ImageButton phonecallButton;
-    private ImageButton emailButton;
+    private LinearLayout mPhonecallButton;
+    private LinearLayout mEmailButton;
+    private  LinearLayout mWebsiteButton;
     private TextView mClubNameTextView;
     private TextView mAddressTextView;
     private TextView mPlaceTextView;
     private TextView mContactPersonTextView;
+    private TextView mTimes;
     private  Club club;
     private ClubDao mClubDao = new ClubDaoImpl();
 
@@ -42,12 +41,14 @@ public class ClubActivity extends AppCompatActivity {
         club = mClubDao.findClubById(clubNumber);
 
         // Initialize views
-        phonecallButton = (ImageButton) findViewById(R.id.phone_call_imageButton);
-        emailButton = (ImageButton) findViewById(R.id.email_imageButton);
+        mPhonecallButton = (LinearLayout) findViewById(R.id.phone_call_linearLayout);
+        mEmailButton = (LinearLayout) findViewById(R.id.email_LinearLayout);
+        mWebsiteButton = (LinearLayout) findViewById(R.id.web_page_LinearLayout);
         mClubNameTextView = (TextView) findViewById(R.id.club_name_textView);
         mAddressTextView = (TextView) findViewById(R.id.address_textView);
         mPlaceTextView = (TextView) findViewById(R.id.place_textView);
         mContactPersonTextView = (TextView) findViewById(R.id.contact_person_textView);
+        mTimes = (TextView) findViewById(R.id.times_textView);
 
         if(club != null) {
             mClubNameTextView.setText(club.getName());
@@ -58,19 +59,32 @@ public class ClubActivity extends AppCompatActivity {
             Toast.makeText(this, "Didn't find any club with id " + clubNumber, Toast.LENGTH_SHORT).show();
         }
 
-        phonecallButton.setOnClickListener(new View.OnClickListener() {
+        mPhonecallButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 makePhoneCallIntent(club.getPhone());
             }
         });
 
-        emailButton.setOnClickListener(new View.OnClickListener() {
+        mEmailButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sendEmailIntent(club.getEmail());
             }
         });
+
+        mWebsiteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebsiteIntent(club.getWebPage());
+            }
+        });
+    }
+
+    private void openWebsiteIntent(String webPage) {
+        Intent webPageIntent = new Intent(Intent.ACTION_VIEW);
+        webPageIntent.setData(Uri.parse(webPage));
+        startActivity(webPageIntent);
     }
 
     private void makePhoneCallIntent(String phoneNumer) {
